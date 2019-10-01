829f5d2c91f5c992f9ff9fc625e9ecfd968aa806
Michael Melgar <melgar.michael@gmail.com>
2019-10-01 17:29:18 -0400

    Set up heroku

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,4 +1,25 @@
-import * APIUtil from '../util/session_api_util.js';
+import * as APIUtil from '../util/session_api_util.js';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const REMOVE_CURRENT_USER = 'REMOVE_CURRENT_USER';
+
+export const receiveCurrentUser = (user) => ({
+    action: RECEIVE_CURRENT_USER,
+    user,
+});
+
+export const removeCurrentUser = () => ({
+    action: REMOVE_CURRENT_USER,
+});
+
+export const signup = (user) => dispatch => {
+    return APIUtil.signup(user).then(user => dispatch(receiveCurrentUser(user)));
+}
+
+export const login = (user) => dispatch => {
+    return APIUtil.signup(user).then(user => dispatch(receiveCurrentUser(user)));
+}
+
+export const logout = () => dispatch => {
+    return APIUtil.logout().then(res => dispatch(removeCurrentUser()));
+}
---
+++ frontend/reducers/sessions_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER } from '../actions/session_actions';
+const nullUser = {id: null};
+const sessionsReducer = (state=nullUser, action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_CURRENT_USER: {
+            return {id: action.user.id}
+        }
+        case REMOVE_CURRENT_USER: {
+            return nullUser;
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+export default sessionsReducer;
